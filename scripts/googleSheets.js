1657e1d1577990adfd155c9cb4387bdddf74f5af
mm-wed <142057416+mm-wed@users.noreply.github.com>
2023-08-11 16:38:14 -0500

    Add files via upload

---
+++ scripts/googleSheets.js
@@ -0,0 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbymGXLPdbzvqu2Rl_YU8cmuFzS0VEiEIE2F2zCejqmuUnh_LoHC24uZxHckuaEYP699/exec'
+        const form = document.forms['submit-to-google-sheet']
+
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+            if (response.ok) {
+                console.log('Success!', response);
+                alert('Email successfully sent!');
+            } else {
+                console.error('Error!', response);
+                alert('Error sending email. Please try again later.');
+            }
+        })
+            .catch(error => console.error('Error!', error.message))
+        })
